a89ea2cf987daaffdc53ff7a2b9a208b2afab607
vadymbug-jun <vadymbug@gmail.com>
2025-11-23 02:26:58 +0300

    added file

---
+++ лаб8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "windows.h"
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x;
+    cout << "Введіть значення x: ";
+    cin >> x;
+
+    double product = 1.0;
+    for (int k = 1; k <= 5; k++) {
+        product *= sin(k * x);
+    }
+    double Z = product + 2;
+    cout << fixed << setprecision(4);
+    cout << "Z = " << Z << endl;
+    system("pause");
+    return 0;
+}
