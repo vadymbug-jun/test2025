9d8dce7ad6f537d88fc1372ee6f649e1e1b21cf8
vadymbug-jun <vadymbug@gmail.com>
2025-11-23 02:33:11 +0300

    edited

--- лаб8.cpp
+++ лаб8.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    //123
     double x;
     cout << "Введіть значення x: ";
     cin >> x;
-
+    
     double product = 1.0;
     for (int k = 1; k <= 5; k++) {
         product *= sin(k * x);
